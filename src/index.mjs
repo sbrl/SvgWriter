618ce88701dda883dde167c295d82553c03ecfdb
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-04-25 15:50:26 +0100

    index.mjs: add main  import & exports

---
+++ src/index.mjs
@@ -0,0 +1,13 @@
+"use strict";
+
+import SvgWriter from './SvgWriter.mjs';
+import Vector2 from './primitives/Vector2.mjs';
+import Rectangle from './primitives/Rectangle.mjs';
+import Triangle from './primitives/Triangle.mjs';
+
+export {
+	SvgWriter,
+	Vector2,
+	Rectangle,
+	Triangle
+};
